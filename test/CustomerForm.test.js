8a11ac380fb5de64ac48087819141b242650e07b
Bianchi446 <josebianchi221js@gmail.com>
2022-04-01 10:58:14 -0300

    renders first name field and extract input field method

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -2,9 +2,6 @@ import React from "react";
 import { createContainer } from "./domManipulators";
 import { CustomerForm } from '../src/CustomerForm'
 
-
-
-
 describe('customerForm', () => {
     let render;
     let container;
@@ -16,19 +13,30 @@ beforeEach(() => {
 
 const form = id => container.querySelector(`form[id="${id}"]`);
 
+const expectToBeInputFieldOfTypeText = formElement => {
+    expect(formElement).not.toBeNull();
+    expect(formElement.tagName).toEqual('INPUT');
+    expect(formElement.type).toEqual('text')
+};
 
+const firstNameField = () => form('customer').elements.firstName;
 
 it('renders a form', () => {
     render(<CustomerForm />);
-    expect(form('customer')).not.toBeNull()
+    expectToBeInputFieldOfTypeText(firstNameField())
 })
 
-it('renders the first name field as a text box', () => {
-    render(<CustomerForm />);
-    const field = form('customer').elements.firstName;
-    expect(field).not.toBeNull();
-    expect(field.tagName).toEqual('INPUT');
-    expect(field.type).toEqual('text');
+    it('renders the first name field as a text box', () => {
+        render(<CustomerForm />);
+        const field = form('customer').elements.firstName;
+        expect(field).not.toBeNull();
+        expect(field.tagName).toEqual('INPUT');
+        expect(field.type).toEqual('text');
 })
+
+    it('includes the existing value for the first name', () => {
+        render(<CustomerForm firstName="Ashley"/>);
+        expect(firstNameField().value).toEqual('Ashley');
+    });
 
 })
