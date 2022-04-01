6d111c3f2af55fef2699f747d44613774cdadbc5
Bianchi446 <josebianchi221js@gmail.com>
2022-04-01 10:30:39 -0300

    Field test

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -20,10 +20,15 @@ const form = id => container.querySelector(`form[id="${id}"]`);
 
 it('renders a form', () => {
     render(<CustomerForm />);
-    expect(
-        form('customer').not.toBeNull()
-    ).not.toBeNull()
+    expect(form('customer')).not.toBeNull()
 })
 
+it('renders the first name field as a text box', () => {
+    render(<CustomerForm />);
+    const field = form('customer').elements.firstName;
+    expect(field).not.toBeNull();
+    expect(field.tagName).toEqual('INPUT');
+    expect(field.type).toEqual('text');
+})
 
 })
